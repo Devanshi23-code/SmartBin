5de43630becb1dee4d3fd924b0b6c24a642893c4
Jaya Vanishitta <j.vanishitta@gmail.com>
2026-04-19 13:56:33 +0530

    added init.py and updated cors properly

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,17 @@ from fastapi import FastAPI
 from typing import List, Optional
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import dotenv
+import dotenv, os
 from fastapi import HTTPException
 from .model import LoginRequest
 # from .config import settings
 dotenv.load_dotenv()
 app = FastAPI()
-
+allowed_origins_str = os.environ.get("ALLOWED_ORIGINS", "")
+origins = allowed_origins_str.split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=dotenv.AllowedOrigins.split(","),
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
